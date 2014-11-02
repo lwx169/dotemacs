7387e4180f2d563a4f4cc7f820a01e07349f45cd
Li wenxiang <lwx169@gmail.com>
2014-11-02 23:15:49 +0800

    Add fill-column-indicator

--- lisp/ui-cfg.el
+++ lisp/ui-cfg.el
@@ -68,5 +68,7 @@
   [term term-color-black term-color-red term-color-green term-color-yellow 
     term-color-blue term-color-magenta term-color-cyan term-color-white])
 
+;;;; fill-colum-indicator
+(require 'fill-column-indicator)
 
 (provide 'ui-cfg)
