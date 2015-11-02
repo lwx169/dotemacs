aabaf74a39b799af3785afd578f72e175c17e06a
Lwxxx <lwx169@gmail.com>
2015-11-02 23:44:20 +0800

    Add plugin: emacs-async

--- init.el
+++ init.el
@@ -4,6 +4,7 @@
 (add-to-list 'load-path "~/.emacs.d/lisp")
 (add-to-list 'load-path "~/.emacs.d/plugins/")
 (add-to-list 'load-path "~/.emacs.d/plugins/helm")
+(add-to-list 'load-path "~/.emacs.d/plugins/async")
 (add-to-list 'load-path "~/.emacs.d/plugins/git-emacs")
 (add-to-list 'load-path "~/.emacs.d/plugins/git-modes")
 (add-to-list 'load-path "~/.emacs.d/plugins/magit")
