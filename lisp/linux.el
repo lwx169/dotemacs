9303581c0151f2111a2f878c63a7219365072695
lwx <lwx@mulangcloud.com>
2019-08-02 19:19:06 +0800

    Fix plugin errors

--- lisp/linux.el
+++ lisp/linux.el
@@ -1,7 +1,7 @@
 ;; setting for Linux
 
 ;;; set font
-(set-default-font "Droid Sans Mono-11")
+;; (set-default-font "Droid Sans Mono-11")
 (set-fontset-font "fontset-default" 'chinese-gb2312 "文泉驿微米黑-12")
 
 (provide 'linux)
--- lisp/prog-cfg.el
+++ lisp/prog-cfg.el
@@ -13,7 +13,7 @@
 ;;                    ac-source-imenu))
 
 ;;; highlight symbo
-(require 'auto-highlight-symbol-config)
+;;(require 'auto-highlight-symbol-config)
 
 ;;; yasnippet
 (require 'yasnippet)
@@ -112,26 +112,6 @@
 ;;;; java
 (require 'lsp-java)
 (add-hook 'java-mode-hook #'lsp)
-
-;; (require 'lsp-javacomp)
-;; (add-hook 'java-mode-hook #'lsp-javacomp-enable)
-;; (use-package lsp-javacomp
-;;   :commands lsp-javacomp-enable
-;;   :init
-;;   (add-hook 'java-mode-hook
-;;             (lambda ()
-;;               ;; Load company-lsp before enabling lsp-javacomp, so that function
-;;               ;; parameter snippet works.
-;;               (require 'company-lsp)
-;;               (lsp-javacomp-enable)
-;;               ;; Use company-lsp as the company completion backend
-;;               (set (make-variable-buffer-local 'company-backends) '(company-lsp))
-;;               ;; Optional company-mode settings
-;;               (set (make-variable-buffer-local 'company-idle-delay) 0.1)
-;;               (set (make-variable-buffer-local 'company-minimum-prefix-length) 1)))
-;;   ;; Optional, make sure JavaComp is installed. See below.
-;;   :config
-;;   (lsp-javacomp-install-server))
 
 ;;;; cmake
 (require 'cmake-mode)
