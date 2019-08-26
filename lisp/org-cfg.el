12c492ef4348b1dfad1bbe014c51a031e22800da
Lwxxx <lwx169@gmail.com>
2019-08-26 16:47:59 +0800

    Disable plantuml-mode config
    目前的配置会导致加载出错，暂时没有在用这个插件，注释掉以后修复。

--- lisp/org-cfg.el
+++ lisp/org-cfg.el
@@ -49,13 +49,14 @@
   ([f7] . cfw:open-org-calendar))
 
 ;;; plantuml
-(require 'plantuml-mode)
-(setq plantuml-jar-path "~/.emacs.d/plugins/plantuml/plantuml.jar")
-(setq org-plantuml-jar-path "~/.emacs.d/plugins/plantuml/plantuml.jar")
-(add-to-list 'auto-mode-alist '("\\.plantuml\\'" . plantuml-mode))
-(add-to-list 'org-src-lang-modes '("plantuml" . plantuml))
-(org-babel-do-load-languages 'org-babel-load-languages
-                             '((plantuml . t)))
+;; (use-package plantuml-mode
+;;   :config
+;;   (setq plantuml-jar-path "~/.emacs.d/plugins/plantuml/plantuml.jar")
+;;   (setq org-plantuml-jar-path "~/.emacs.d/plugins/plantuml/plantuml.jar")
+;;   (add-to-list 'auto-mode-alist '("\\.plantuml\\'" . plantuml-mode))
+;;   (add-to-list 'org-src-lang-modes '("plantuml" . plantuml))
+;;   (org-babel-do-load-languages 'org-babel-load-languages
+;;                                '((plantuml . t))))
 
 ;;; org screenshot
 (defun org-screenshot ()
