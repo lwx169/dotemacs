2eb506b119106eea51cc3b5fe71fcb4090874e98
Lwxxx <lwx169@gmail.com>
2020-07-21 18:33:58 +0800

    Do not set font under linux
    set in local-cfg.el

--- lisp/linux.el
+++ lisp/linux.el
@@ -2,6 +2,6 @@
 
 ;;; set font
 ;; (set-default-font "Droid Sans Mono-11")
-(set-fontset-font "fontset-default" 'chinese-gb2312 "文泉驿微米黑-12")
+;; (set-fontset-font "fontset-default" 'chinese-gb2312 "文泉驿微米黑-12")
 
 (provide 'linux)
