ebba7b93c297f601ff389e3106dcd367f6080da5
lwx <lwx@lwxdeMac-mini.local>
2021-08-07 19:02:00 +0800

    Support use chez in geiser

--- lisp/prog-cfg.el
+++ lisp/prog-cfg.el
@@ -204,8 +204,8 @@
   (setq indent-tabs-mode nil))
 
 ;;;; scheme
-(setq scheme-program-name "chez")
-(setq geiser-chez-binary "chez")
+;; (setq scheme-program-name "chez")
+;; (setq geiser-chez-binary "chez")
 (setq geiser-active-implementations '(chez))
 
 ;;; haxe
