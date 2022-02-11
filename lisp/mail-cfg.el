61e0cb6b75dc06de3afad04702c3198dd811e7f0
lwx <lwx169@gmail.com>
2022-02-11 11:32:03 +0800

    Update mail config

--- lisp/mail-cfg.el
+++ lisp/mail-cfg.el
@@ -10,7 +10,7 @@
         (:name "unread" :query "tag:unread" :key "u" :sort-order newest-first :search-type tree)
         (:name "linux-kernel" :query "to:linux-kernel@vger.kernel.org tag:unread" :key "l" :sort-order newest-first :search-type tree)
         (:name "kernelnewbies" :query "to:kernelnewbies@kernelnewbies.org tag:unread" :key "kn" :sort-order newest-first :search-type tree)
-        (:name "org-mode" :query "to:emacs-orgmode@gnu.org tag:unread" :key "o" :sort-order newest-first :search-type tree)
+        (:name "org-mode" :query "tag:unread and tag:lists/emacs-orgmode" :key "o" :sort-order newest-first :search-type tree)
         (:name "ceph" :query "to:*@ceph.io tag:unread" :key "c" :sort-order newest-first :search-type tree)
         (:name "mulang" :query "path:/Mulang/ tag:unread" :key "m" :sort-order newest-first :search-type tree)
         (:name "tracked" :query "tag:tracked" :key "t" :sort-order newest-first :search-type tree)
