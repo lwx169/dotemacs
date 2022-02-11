5d9b63541319c91c5e27813a18d04543ac22b27e
lwx <lwx169@gmail.com>
2022-02-11 22:42:04 +0800

    Disable imenu integration of citre

--- lisp/prog-cfg.el
+++ lisp/prog-cfg.el
@@ -121,7 +121,9 @@
    ;; By default, when you open any file, and a tags file can be found for it,
    ;; `citre-mode' is automatically enabled.  If you only want this to work for
    ;; certain modes (like `prog-mode'), set it like this.
-   citre-auto-enable-citre-mode-modes '(prog-mode)))
+   citre-auto-enable-citre-mode-modes '(prog-mode))
+  :custom
+  (citre-enable-imenu-integration nil "disable imenu interactive"))
 
 (use-package citre-global
   :ensure nil
